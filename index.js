bf956e70c31269599fc91110dc785eae87401f7f
Thomas Bender <thomasmatthewbender@gmail.com>
2022-03-16 13:12:25 -0400

    commit tree 85aed4a45f8cee738969822061454288812d3d77

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!"
+  } else if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks." 
+  } else if (feet > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  // return ("NYC" ? "Ok, sounds good." : "No go.")
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
+console.log(ternaryCheckCity("NYC"))
+console.log(ternaryCheckCity("Pittsburgh"))
+  //let NYC = true ? "Ok, sounds good." : "No go."
+  // Write your code here!
+  //console.log(NYC)
+
+
+
+  function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip) {
+    case "generous":
+    return("Thank you so much.");
+    break;
+    case "not as generous":
+    return("Thank you.");
+    break;
+    default:
+    return "Bye.";
+  }
+  }
+  console.log(switchOnCharmFromTip("generous"))
+  console.log(switchOnCharmFromTip("not as generous"))
+  console.log(switchOnCharmFromTip())
+
+
+
+
+
